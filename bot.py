2673a1cfb4d321a06f77aa83e3686ac0216880e0
RUBAN KARTHICK V <124701104+rubankarthick@users.noreply.github.com>
2023-08-23 20:58:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-#(©)Codexbotz
 
 from aiohttp import web
 from plugins import web_server
@@ -55,7 +54,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/RUBAN9124")
         self.LOGGER(__name__).info(f""" \n\n 
 ████████╗███████╗░█████╗░███╗░░░███╗  ██╗░░░██╗██╗░░██╗██████╗░
 ╚══██╔══╝██╔════╝██╔══██╗████╗░████║  ██║░░░██║██║░░██║██╔══██╗
